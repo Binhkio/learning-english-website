50e4bd8f7a3d9c8b0cbc782e1f2b417a9bc8f39d
Binhkio <quan.va205115@gmail.com>
2023-04-12 11:13:10 +0700

    create appContext

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,11 +8,14 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import AppContextProvider from './context/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </React.StrictMode>,
 );
 
