a34dd087f4e66bec3beeed33c73835617a19d94a
Binhkio <quan.va205115@gmail.com>
2023-05-06 13:05:32 +0700

    mongodb advance

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
+const ROLE = {
+  USER: 0,
+  ADMIN: 1,
+};
 
 const User = mongoose.Schema(
   {
     email: String,
     name: String,
     password: String,
-    role: Number,
-    lessons: Array,
-    quizzes: Array,
+    // role: Number, // 0: User, 1 :Admin
+    role: { type: Number, enum: Object.values(ROLE) },
+    lessons: [{type: ObjectId, ref: "Lesson"}],
+    quizzes: [{type: ObjectId, ref: "Quiz"}],
     status: Number
   },
   {
